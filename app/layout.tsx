c21d30698ef27fcb8cdafb00d77a183ea57a3bb2
Jesse <jessebeke10@gmail.com>
2024-09-01 22:18:07 +0100

    feat: added static and dynamic metadata for each important route

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,30 @@ const inter = Inter({ subsets: ["latin"] });
 const syne = Syne({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Sofa | Deals",
-  description: "Shop deals on furniture.",
+  title: "Sofa Store | Home",
+  description:
+    "Welcome to Sofa Store - Your destination for the best furniture deals.",
+  openGraph: {
+    title: "Sofa Store | Home",
+    description: "Discover the best deals on furniture at Sofa Store.",
+    url: "https://sofa-beta.vercel.app",
+    siteName: "Sofa Store",
+    images: [
+      {
+        url: "/sofa-og.png",
+        width: 1200,
+        height: 630,
+        alt: "Sofa Store - Best furniture deals",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Sofa Store | Home",
+    description: "Discover the best deals on furniture at Sofa Store.",
+    images: ["/sofa-og.png"],
+  },
 };
 
 export default function RootLayout({
--- app/product/[id]/layout.tsx
+++ app/product/[id]/layout.tsx
@@ -1,14 +1,38 @@
 import type { Metadata } from "next";
 import { Inter, Syne } from "next/font/google";
-// import "../globals.css";
+import { db } from "@/lib/db"; // Ensure the path to your db is correct
+import { notFound } from "next/navigation";
 
 const inter = Inter({ subsets: ["latin"] });
 const syne = Syne({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Sofa | Deals",
-  description: "Shop deals on furniture.",
-};
+export async function generateMetadata({
+  params,
+}: {
+  params: { id: string };
+}): Promise<Metadata> {
+  const productId = parseInt(params.id);
+  const product = await db.products.get(productId);
+
+  if (!product) {
+    return notFound(); // Return a 404 if the product is not found
+  }
+
+  return {
+    title: `${product.name} | Deals`,
+    description: `Shop the best deals on ${product.name}.`,
+    openGraph: {
+      title: `${product.name} | Deals`,
+      description: `Shop the best deals on ${product.name}.`,
+      images: [
+        {
+          url: product.image,
+          alt: product.name,
+        },
+      ],
+    },
+  };
+}
 
 export default function ProductLayout({
   children,
