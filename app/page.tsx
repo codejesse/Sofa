0ca362e4ad24d3b06ec54852749fa007ae74e522
Jesse <jessebeke10@gmail.com>
2024-09-01 13:33:24 +0100

    feat: modified the hero section

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import dynamic from "next/dynamic";
 import Image from "next/image";
 import HeroImage from "./hero.png";
+import { Button } from "@/components/ui/button";
 
 const ProductList = dynamic(() => import("../components/ProductList"), {
   //👨🏿‍💻 Add a custom loader
@@ -19,10 +20,12 @@ export default function Home() {
           content="sofa, furniture, home, decoration interior"
         />
       </Head>
-      <div className="absolute inset-0 -z-10 h-[400px] w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]"></div>
+      <div className="absolute inset-0 -z-10 h-[600px] w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]"></div>
       <div className="flex flex-col lg:flex-row gap-4 mx-auto text-center lg:m-20">
-        <div className="flex flex-col">
-          <h1 className="font-semibold text-5xl lg:text-left">Our Product List</h1>
+        <div className="flex flex-col mx-8 lg:mx-auto">
+          <h1 className="font-semibold text-5xl lg:text-left">
+            Our Product List
+          </h1>
           <p className="text-[16px] text-[#9C9C9C] lg:text-left">
             We provide selling or buying quality and marketable furniture and
             have our own aesthetic in our furniture.
@@ -30,11 +33,12 @@ export default function Home() {
           <p className="text-[16px] text-[#9C9C9C] lg:text-left">
             check out our latest furniture listings below
           </p>
+          <Button className="mt-4 lg:w-8/12">Get started</Button>
         </div>
         <Image
-          className="w-full h-fit lg:mt-[-50px] ml-20 lg:ml-20"
+          className="w-full h-fit lg:mt-[-50px] mr-[-300px] lg:ml-20"
           src={HeroImage}
-          width={600}
+          width={800}
           height={600}
           alt="hero-image"
         />
